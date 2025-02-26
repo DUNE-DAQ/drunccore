9fd8c94122dfecfec09f16fcd44846d809e9043c
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-02-26 23:02:52 +0100

    make the apps correct

--- src/drunc/apps/__main_session_manager__.py
+++ src/drunc/apps/__main_session_manager__.py
@@ -1,12 +1,15 @@
 from drunc.session_manager.interface.session_manager import session_manager_cli
-from drunc.utils.utils import print_traceback
+from drunc.utils.utils import print_traceback, get_logger, setup_root_logger, create_logger_handler
 
 def main():
     try:
         session_manager_cli()
-    except Exception:
-        print_traceback()
-
+    except Exception as e:
+        setup_root_logger("INFO")
+        log = get_logger("session_manager")
+        create_logger_handler(rich_handler=True)
+        log.error("[red bold]:fire::fire: Exception thrown :fire::fire:")
+        print_traceback(e)
 
 if __name__ == "__main__":
     main()
