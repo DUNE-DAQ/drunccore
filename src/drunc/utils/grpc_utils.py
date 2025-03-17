f09eeb27fb41416e473c6a2e2694644d36c8914c
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-03-17 17:46:12 +0100

    thats a long comment

--- src/drunc/utils/grpc_utils.py
+++ src/drunc/utils/grpc_utils.py
@@ -39,9 +39,7 @@ def unpack_any(data, format):
 
 def unpack_request_data_to(data_type=None, pass_token=False):
     def decor(cmd):
-        @functools.wraps(
-            cmd
-        )  # this nifty decorator of decorator (!) is nicely preserving the cmd.__name__ (i.e. signature)
+        @functools.wraps(cmd)
         def unpack_request(obj, request):
             log = get_logger("utils.unpack_request_data_to_decorator")
             log.debug("Entering")
@@ -82,9 +80,7 @@ def unpack_request_data_to(data_type=None, pass_token=False):
 
 def async_unpack_request_data_to(data_type=None, pass_token=False):
     def decor(cmd):
-        @functools.wraps(
-            cmd
-        )  # this nifty decorator of decorator (!) is nicely preserving the cmd.__name__ (i.e. signature)
+        @functools.wraps(cmd)
         async def unpack_request(obj, request):
             log = get_logger("utils.async_unpack_request_data_to_decorator")
             log.debug("Executing wrapped function")
@@ -124,9 +120,7 @@ def pack_response(cmd, with_children_responses=False):
         "This function is deprecated, pack your responses yourself"
     )
 
-    @functools.wraps(
-        cmd
-    )  # this nifty decorator of decorator (!) is nicely preserving the cmd.__name__ (i.e. signature)
+    @functools.wraps(cmd)
     def pack_response(obj, *arg, **kwargs):
         log = get_logger("utils.pack_response_decorator")
         log.debug("Executing wrapped function")
@@ -160,9 +154,7 @@ def async_pack_response(cmd, with_children_responses=False):
         "This function is deprecated, pack your responses yourself"
     )
 
-    @functools.wraps(
-        cmd
-    )  # this nifty decorator of decorator (!) is nicely preserving the cmd.__name__ (i.e. signature)
+    @functools.wraps(cmd)
     async def pack_response(obj, *arg, **kwargs):
         log = get_logger("utils.async_pack_response_decorator")
         log.debug("Executing wrapped function")
