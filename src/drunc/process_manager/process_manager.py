285091164b907e0b90ab36f25dd918d958ee0b54
wanyunSu <wanyun.su@cern.ch>
2025-04-07 12:19:30 +0200

    fix for getting None type

--- src/drunc/process_manager/process_manager.py
+++ src/drunc/process_manager/process_manager.py
@@ -92,7 +92,7 @@ class ProcessManager(abc.ABC, ProcessManagerServicer):
             data=self.configuration.data.authoriser, type=ConfTypes.PyObject
         )
 
-        self.opmon_publisher = self.configuration.data.opmon_publisher
+        self.opmon_publisher = getattr(self.configuration.data.opmon_publisher,None)
         opmon_sleep_time = getattr(self.configuration.data, "opmon_sleep_time", 5)
         self.authoriser = DummyAuthoriser(dach, SystemType.PROCESS_MANAGER)
 
